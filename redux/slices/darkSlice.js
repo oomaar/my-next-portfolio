f6d7b29a8513e40923402a59bb3928a2e552cf54
Omar <omaar_5@hotmail.com>
2021-09-19 23:57:26 +0200

    setup: redux store and slice

---
+++ redux/slices/darkSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const darkSlice = createSlice({
+    name: "dark",
+    initialState: {
+        darkState: "light"
+    },
+    reducers: {
+        setDarkMode: (state, action) => {
+            state.darkState = action.payload;
+        }
+    }
+});
+
+export const darkValue = state => state.dark.darkState;
+export const { setDarkMode } = darkSlice.actions;
+
+export default darkSlice.reducer;
---
+++ redux/store.js
@@ -0,0 +1,36 @@
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import darkReducer from "./slices/darkSlice";
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+    dark: darkReducer
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export let persistor = persistStore(store);

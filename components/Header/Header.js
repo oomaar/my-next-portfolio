1afc79923e85b66bc568bbcb3c54ae33591a6bf6
Omar <omaar_5@hotmail.com>
2021-09-20 23:47:51 +0200

    style: Eye follow mouse

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,57 @@
 import {
     HeaderTag,
+    LinksList,
+    Link,
+    EyeContainer,
+    Eye,
+    Ball,
+    Lashes,
+    Lash,
+    SocialBox,
+    SocialLink,
 } from "./styledHeader";
 
 export const Header = () => {
+    const array = ['Work', 'About', 'Education', 'Contact'];
+    const links = array.map((link, i) => (
+        <li key={i}>
+            <Link>{link}</Link>
+        </li>
+    ));
+
+    let ball = document.getElementsByClassName("ball");
+
+    document.onmousemove = function () {
+        let x = event.clientX * 100 / window.innerWidth + "%";
+        let y = event.clientY * 100 / window.innerHeight + "%";
+
+        for (var i = 0; i < 1; i++) {
+            ball[i].style.left = x;
+            ball[i].style.top = y;
+            ball[i].style.transform = "translate(-" + x + ", -" + y + ")";
+        };
+    };
+
     return (
         <HeaderTag>
-            Header component
+            <LinksList>
+                {links}
+            </LinksList>
+            <EyeContainer>
+                <Lashes>
+                    <Lash className="lash__1"></Lash>
+                    <Lash className="lash__2"></Lash>
+                    <Lash className="lash__3"></Lash>
+                </Lashes>
+                <Eye>
+                    <Ball className="ball"></Ball>
+                </Eye>
+            </EyeContainer>
+            <SocialBox>
+                <SocialLink>Github</SocialLink>
+                <SocialLink>Whatsapp</SocialLink>
+            </SocialBox>
+
         </HeaderTag>
     );
 };
--- components/Header/styledHeader.js
+++ components/Header/styledHeader.js
@@ -1,3 +1,89 @@
 import styled from 'styled-components';
 
-export const HeaderTag = styled.header``;
+export const HeaderTag = styled.header`
+  background-color: #111;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LinksList = styled.ul`
+  display: flex;
+`;
+
+export const Link = styled.a``;
+
+export const Lashes = styled.div`
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 0.3rem;
+  position: absolute;
+  top: -0.8rem;
+  display: none;
+  transition: all 0.4s cubic-bezier(.175,.885,.32,1.275);
+  
+  .lash__1 {
+    transform: rotate(-35deg);
+  }
+
+  .lash__2 {
+    height: 11px;
+  }
+
+  .lash__3 {
+    transform: rotate(40deg);
+  }
+`;
+
+export const Lash = styled.div`
+  width: 4px;
+  height: 9px;
+  background-color: #fff;
+  border-radius: 25%;
+  transition: all 0.4s cubic-bezier(.175,.885,.32,1.275);
+`;
+
+export const Eye = styled.div`
+  width: 28px;
+  height: 20px;
+  background: #fff;
+  display: inline-block;
+  border-radius: 50%;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Ball = styled.div`
+  width: 12px;
+  height: 12px;
+  background: #000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+`;
+
+
+export const EyeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  cursor: pointer;
+
+  :hover ${Lashes} {
+    display: flex;
+  }
+
+  :hover ${Ball} {
+    width: 15px;
+    height: 15px;
+    transition: all 0.4s cubic-bezier(.175,.885,.32,1.275);
+  }
+`;
+
+export const SocialBox = styled.div``;
+
+export const SocialLink = styled.div``;

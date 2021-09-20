c128c91a1c3d986f37c4bcdbf5a068840ded822a
Omar <omaar_5@hotmail.com>
2021-09-20 23:40:38 +0200

    refactor: index page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Headtag } from "../global";
-import { darkValue, setDarkMode } from "../redux/slices/darkSlice";
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const darkThemeState = useSelector(darkValue);
-
-  const handleDarkMode = () => darkThemeState === "light" ? (
-    dispatch(setDarkMode("dark"))
-  ) : (
-    dispatch(setDarkMode("light"))
-  );
-
   return (
     <div>
       <Headtag title="Omar's Portfolio | Resume" />
 
-      <h1 onClick={() => handleDarkMode()}>Build something crazy</h1>
+      <h1>Build something crazy</h1>
     </div>
   )
 };

1205f5204fa72eb89d04d8b84788dacb651e5aac
Omar <omaar_5@hotmail.com>
2021-10-21 00:08:22 +0200

    style: Footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,58 @@
+import { SiGmail, SiWhatsapp } from "react-icons/si";
+import { FiGithub } from "react-icons/fi";
+import { GrInstagram } from "react-icons/gr";
+import { RiMessengerLine, RiLinkedinLine, RiTwitterLine, RiFacebookLine } from "react-icons/ri";
 import {
     FooterContainer,
+    SubContainer,
+    Info,
+    Title,
+    SubTitle,
+    Icons,
+    Copy,
 } from "./styledFooter";
+import { Icon } from "../../global";
 
 export const Footer = () => {
     return (
         <FooterContainer>
-            Footer
+            <SubContainer>
+                <Info>
+                    <Title>Omar Hassan</Title>
+                    <SubTitle>Programmer, Web Developer</SubTitle>
+                </Info>
+
+                <Icons>
+                    <Icon>
+                        <FiGithub />
+                    </Icon>
+                    <Icon>
+                        <RiFacebookLine />
+                    </Icon>
+                    <Icon>
+                        <RiMessengerLine />
+                    </Icon>
+                    <Icon>
+                        <GrInstagram />
+                    </Icon>
+                    <Icon>
+                        <RiLinkedinLine />
+                    </Icon>
+                    <Icon>
+                        <RiTwitterLine />
+                    </Icon>
+                    <Icon>
+                        <SiGmail />
+                    </Icon>
+                    <Icon>
+                        <SiWhatsapp />
+                        {/* <FaWhatsapp /> */}
+                    </Icon>
+                </Icons>
+            </SubContainer>
+            <Copy>
+                &#169; <span>Omar's</span> Portfolio | Powered by NextJS
+            </Copy>
         </FooterContainer>
     );
 };
--- components/Footer/styledFooter.js
+++ components/Footer/styledFooter.js
@@ -1,3 +1,60 @@
 import styled from 'styled-components';
+import { mdScreen, xlScreen } from '../../global';
 
-export const FooterContainer = styled.footer``;
+export const FooterContainer = styled.footer`
+  background-color: ${({ theme }) => theme.colors.footerColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SubContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media screen and (max-width: ${xlScreen}) {
+    padding: 0 0.5rem;
+  }
+
+  @media screen and (max-width: ${mdScreen}) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const Info = styled.div`
+  @media screen and (max-width: ${mdScreen}) {
+    text-align: center;
+  }
+`;
+
+export const Title = styled.h3`
+  font-size: ${({ theme }) => theme.fontSize.h1FontSize};
+  color: ${({ theme }) => theme.colors.titleColor};
+  margin: 1rem 0;
+`;
+
+export const SubTitle = styled.p`
+  color: ${({ theme }) => theme.colors.textColor};
+  font-size: ${({ theme }) => theme.fontSize.normalFontSize};
+`;
+
+export const Icons = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem;
+  margin: 1rem 0;
+`;
+
+export const Copy = styled.h6`
+  font-size: ${({ theme }) => theme.fontSize.smallFontSize};
+  color: ${({ theme }) => theme.colors.textColor};
+
+  span {
+    color: ${({ theme }) => theme.colors.linkColor};
+    cursor: pointer;
+  }
+`;

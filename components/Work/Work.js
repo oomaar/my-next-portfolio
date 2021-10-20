c40b7665ad01a31aa8e95758cd56d237647661f3
Omar <omaar_5@hotmail.com>
2021-10-20 18:08:29 +0200

    style: Work Component

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -1,11 +1,112 @@
+import { Container, Section, Title } from "../../global";
+import { FaGithub } from "react-icons/fa";
+import { useState } from "react";
 import {
-    Container,
+    Navigation,
+    NavLinks,
+    CardsContainer,
+    Card,
+    CardImg,
+    CardSubContainer,
+    CardTitle,
+    CardDescription,
+    CardFooter,
+    CardFooterIcon,
+    CardFooterLink,
 } from "./styledWork";
 
-export const Work = () => {
+export const Work = ({ data }) => {
+    const [category, setCategory] = useState("all");
+
+    const clonesArray = data.clones.map(clone => (
+        <Card key={clone.id}>
+            <CardImg src={clone.image} />
+            <CardTitle>{clone.title}</CardTitle>
+            <CardSubContainer>
+                <CardDescription>{clone.description}</CardDescription>
+                <CardFooter>
+                    <CardFooterIcon href="#">
+                        <FaGithub />
+                    </CardFooterIcon>
+                    <CardFooterLink href={clone.url}>Live Preview</CardFooterLink>
+                </CardFooter>
+            </CardSubContainer>
+        </Card>
+    ));
+
+    const schoolArray = data.school.map(school => (
+        <Card key={school.id}>
+            <CardImg src={school.image} />
+            <CardTitle>{school.title}</CardTitle>
+            <CardSubContainer>
+                <CardDescription>{school.description}</CardDescription>
+                <CardFooter>
+                    <CardFooterIcon href="#">
+                        <FaGithub />
+                    </CardFooterIcon>
+                    <CardFooterLink href={school.url}>Live Preview</CardFooterLink>
+                </CardFooter>
+            </CardSubContainer>
+        </Card>
+    ));
+
+    const freeLanceArray = data.freeLance.map(free => (
+        <Card key={free.id}>
+            <CardImg src={free.image} />
+            <CardTitle>{free.title}</CardTitle>
+            <CardSubContainer>
+                <CardDescription>{free.description}</CardDescription>
+                <CardFooter>
+                    <CardFooterIcon href="#">
+                        <FaGithub />
+                    </CardFooterIcon>
+                    <CardFooterLink href={free.url}>Live Preview</CardFooterLink>
+                </CardFooter>
+            </CardSubContainer>
+        </Card>
+    ));
+
     return (
-        <Container>
-            Work Component
-        </Container>
+        <Section id="work">
+            <Container>
+                <Title>Works</Title>
+
+                <Navigation>
+                    <NavLinks
+                        onClick={() => setCategory("all")}
+                    >
+                        All
+                    </NavLinks>
+                    <NavLinks
+                        onClick={() => setCategory("clones")}
+                    >
+                        Clones
+                    </NavLinks>
+                    <NavLinks
+                        onClick={() => setCategory("school")}
+                    >
+                        School
+                    </NavLinks>
+                    <NavLinks
+                        onClick={() => setCategory("free")}
+                    >
+                        Freelance
+                    </NavLinks>
+                </Navigation>
+
+                <CardsContainer>
+                    {category === "all" && (
+                        <>
+                            {clonesArray}
+                            {schoolArray}
+                            {freeLanceArray}
+                        </>
+                    )}
+                    {category === "clones" && clonesArray}
+                    {category === "school" && schoolArray}
+                    {category === "free" && freeLanceArray}
+                </CardsContainer>
+            </Container>
+        </Section>
     );
 };
--- components/Work/styledWork.js
+++ components/Work/styledWork.js
@@ -1,3 +1,100 @@
 import styled from 'styled-components';
+import { mdScreen, xlScreen, lgScreen } from '../../global';
+import { tansition_04 } from '../../global/styles/GlobalStyle';
 
-export const Container = styled.div``;
+export const Navigation = styled.nav`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  width: 100%;
+
+  @media screen and (max-width: ${mdScreen}) {
+    gap: 0;
+  }
+`;
+
+export const NavLinks = styled.button`
+  outline: 0;
+  border: 0;
+  border-bottom: 4px solid transparent;
+  background: transparent;
+  color: ${({ theme }) => theme.colors.titleColor};
+  font-size: clamp(1.2rem, 2vw, 2rem);
+  transition: ${tansition_04};
+
+  :hover {
+    border-bottom: ${({ theme }) => theme.colors.borderColor};
+  }
+`;
+
+export const CardsContainer = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 2rem;
+  overflow-y: scroll;
+
+  @media screen and (max-width: ${xlScreen}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: ${lgScreen}) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Card = styled.div`
+  background: ${({ theme }) => theme.colors.cardColor};
+  height: 500px;
+  border-radius: 1rem;
+`;
+
+export const CardImg = styled.img`
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  cursor: pointer;
+  height: 265px;
+`;
+
+export const CardSubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 0.5rem;
+`;
+
+export const CardTitle = styled.h3`
+  text-align: center;
+  font-size: 1.6rem;
+  margin: 0;
+  color: ${({ theme }) => theme.colors.titleColor};
+`;
+
+export const CardDescription = styled.p`
+  font-size: 1.1rem;
+  color: ${({ theme }) => theme.colors.textColor};
+  height: 100px;
+`;
+
+export const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 1px solid ${({ theme }) => theme.colors.textColor};
+  padding: 0.5rem 0;
+`;
+
+export const CardFooterIcon = styled.a`
+  font-size: 2rem;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.titleColor};
+`;
+
+export const CardFooterLink = styled.a`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.textColorLight};
+  letter-spacing: 0.1rem;
+`;

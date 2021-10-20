060321abf2332f1d67585951dc8b338fa2c03f33
Omar <omaar_5@hotmail.com>
2021-10-20 22:10:15 +0200

    style: Contact

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,6 +1,14 @@
 import { Container, Section, Title } from "../../global";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { AiFillPhone } from "react-icons/ai";
 import {
     ContactContainer,
+    ContactCard,
+    ContactTitle,
+    ContactIcon,
+    ContactText,
 } from "./styledContact";
 
 export const Contact = ({ data }) => {
@@ -10,7 +18,35 @@ export const Contact = ({ data }) => {
                 <Title>Contact Me</Title>
 
                 <ContactContainer>
-                    Contact Container
+                    <ContactCard>
+                        <ContactIcon>
+                            <FaFacebookMessenger />
+                        </ContactIcon>
+                        <ContactTitle>Message</ContactTitle>
+                        <ContactText>Facebook Messanger</ContactText>
+                    </ContactCard>
+                    <ContactCard>
+                        <ContactIcon>
+                            <IoLogoWhatsapp />
+                        </ContactIcon>
+                        <ContactTitle>Chat</ContactTitle>
+                        <ContactText>Whatsapp: (+02)-011-4012-5555</ContactText>
+                    </ContactCard>
+                    <ContactCard>
+                        <ContactIcon>
+                            <MdEmail />
+                        </ContactIcon>
+                        <ContactTitle>Email</ContactTitle>
+                        <ContactText>omaar_5@hotmail.com</ContactText>
+                    </ContactCard>
+                    <ContactCard>
+                        <ContactIcon>
+                            <AiFillPhone />
+                        </ContactIcon>
+                        <ContactTitle>Call</ContactTitle>
+                        <ContactText>(+02)-011-4012-5555</ContactText>
+                    </ContactCard>
+
                 </ContactContainer>
             </Container>
         </Section>
--- components/Contact/styledContact.js
+++ components/Contact/styledContact.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
+import { lgScreen, xlScreen } from '../../global';
 
-export const ContactContainer = styled.div``;
+export const ContactContainer = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(4, 1fr);
+  text-align: center;
+
+  @media screen and (max-width: ${xlScreen}) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
+  }
+
+  @media screen and (max-width: ${lgScreen}) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const ContactCard = styled.div`
+  background-color: ${({ theme }) => theme.colors.cardColor};
+  border-radius: 1rem;
+  padding: 1rem;
+`;
+
+export const ContactIcon = styled.div`
+  font-size: ${({ theme }) => theme.fontSize.bigFontSize};
+`;
+
+export const ContactTitle = styled.p`
+  color: ${({ theme }) => theme.colors.titleColor};
+  font-size: ${({ theme }) => theme.fontSize.h1FontSize};
+`;
+
+export const ContactText = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.normalFontSize};
+`;

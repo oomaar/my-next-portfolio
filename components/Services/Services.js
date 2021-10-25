8c652321bd8194f8d7c9aea23f971cac3bd54dff
Omar <omaar_5@hotmail.com>
2021-10-25 23:58:31 +0200

    style: Services Component

--- components/Services/Services.js
+++ components/Services/Services.js
@@ -1,16 +1,90 @@
 import { Container, Section, Title } from "../../global";
+import { RiBracesFill } from "react-icons/ri";
+import { BsServer } from "react-icons/bs";
+import { CgDesignmodo } from "react-icons/cg";
+import { HiOutlineArrowRight } from "react-icons/hi";
+import { useState } from "react";
+import { ServiceModal } from "..";
 import {
     ServicesContainer,
+    SubContainer,
+    TitleContainer,
+    ServicesTitle,
+    Icon,
+    ViewLink,
 } from "./styledServices";
 
-export const Services = () => {
+export const Services = ({ data }) => {
+    const modalData = data.modals;
+    const [showModal, setShowModal] = useState(false);
+    const [modal, setModal] = useState("");
+
+    const handleModal = skill => {
+        setShowModal(true);
+        setModal(skill);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setModal("");
+    };
+
     return (
         <Section>
             <Container>
                 <Title>Services</Title>
-
+                {showModal && (
+                    <ServiceModal
+                        handleCloseModal={handleCloseModal}
+                        modal={modal}
+                        data={modalData}
+                    />
+                )}
                 <ServicesContainer>
-                    Services Component
+                    <SubContainer>
+                        <TitleContainer>
+                            <Icon>
+                                <RiBracesFill />
+                            </Icon>
+                            <ServicesTitle>Front-End</ServicesTitle>
+                        </TitleContainer>
+                        <ViewLink onClick={() => handleModal("front")}>
+                            <span>View More</span>
+                            <Icon>
+                                <HiOutlineArrowRight />
+                            </Icon>
+                        </ViewLink>
+                    </SubContainer>
+
+                    <SubContainer>
+                        <TitleContainer>
+                            <Icon>
+                                <BsServer />
+                            </Icon>
+                            <ServicesTitle>Back-End</ServicesTitle>
+                        </TitleContainer>
+                        <ViewLink onClick={() => handleModal("back")}>
+                            <span>View More</span>
+                            <Icon>
+                                <HiOutlineArrowRight />
+                            </Icon>
+                        </ViewLink>
+                    </SubContainer>
+
+                    <SubContainer>
+                        <TitleContainer>
+                            <Icon>
+                                <CgDesignmodo />
+                            </Icon>
+                            <ServicesTitle>Design</ServicesTitle>
+                        </TitleContainer>
+                        <ViewLink onClick={() => handleModal("design")}>
+                            <span>View More</span>
+                            <Icon>
+                                <HiOutlineArrowRight />
+                            </Icon>
+                        </ViewLink>
+                    </SubContainer>
                 </ServicesContainer>
             </Container>
         </Section>
--- components/Services/styledServices.js
+++ components/Services/styledServices.js
@@ -1,3 +1,66 @@
 import styled from 'styled-components';
 
-export const ServicesContainer = styled.div``;
+export const ServicesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* gap: 1rem; */
+  width: 100%;
+  position: relative;
+  z-index: 1;
+`;
+
+export const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.bodyColor};
+  border-radius: 1rem;
+  gap: 1rem;
+  padding: 1rem;
+  max-width: 300px;
+  width: 100%;
+
+  :nth-child(1) {
+    align-self: flex-start;
+  }
+
+  :nth-child(2) {
+    align-self: center;
+  }
+
+  :nth-child(3) {
+    align-self: flex-end;
+  }
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ServicesTitle = styled.h3`
+  font-size: clamp(1.5rem, 2vw, 2rem);
+  color: ${({ theme }) => theme.colors.titleColor};
+`;
+
+export const Icon = styled.div`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.colors.iconColor};
+  transition: all 0.3s ease-in;
+`;
+
+export const ViewLink = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  border: 0;
+  outline: 0;
+  background: transparent;
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.iconColor};
+  transition: all 0.3s ease-in;
+
+  :hover ${Icon} {
+    transform: translateX(10px);
+  }
+`;
